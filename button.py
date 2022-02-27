7528a9e3e6893a71cc1bea57579c20eb30043dba
ericly <ericl5525@gmail.com>
2022-02-26 20:33:20 -0500

    hey

--- button.py
+++ button.py
@@ -10,7 +10,7 @@ next_btn = pg.image.load('next_btn.png').convert_alpha()
 class Button:
     def __init__(self, x, y, image):
         self.image = image
-        self.rect - self.image.get_rect()
+        self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
     
     def draw(self):
